ea07b4ab635cdbeadef9c0be533cb8309019546b
manlikemaleek <malikagboola3@gmail.com>
2022-04-22 01:30:03 +0100

    push

---
+++ src/components/About.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>
+        <div>
+            <div>
+                <h2>Trusted by Developers across the world</h2>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aut sunt laborum delectus.
+                     Accusamus et, perferendis laboriosam cupiditate eligendi obcaecati ducimus.</p>
+
+
+            </div>
+            <div>
+                <div>
+                    <p>100%</p>
+                    <p>Completion</p>
+                </div>
+
+                <div>
+                    <p>24/7</p>
+                    <p>Delivery</p>
+                </div>
+
+                <div>
+                    <p>100K</p>
+                    <p>Transactions</p>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default About
---
+++ src/components/hero.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import {
+    CloudUploadIcon,
+    DatabaseIcon,
+    PaperAirplaneIcon,
+    ServerIcon,
+} from '@heroicons/react/solid'
+
+import bgImage from '../assets/cyber-bg.png'
+
+const hero = () => {
+  return (
+    <div className='w-full h-screen bg-zinc-200 flex flex-col justify-betwween '>
+
+        <div className='grid md:grid-cols-2 max-w-[1240px] m-auto'>
+            <div className=' flex flex-col justify-center md:items-start w-full px-2 py-8'>
+
+
+            <p className='text-2xl'>Website Design & Maintainance</p>
+            <h1 className='py-3 text-5xl md:text-7xl font-bold'>Website Management</h1>
+            <p className='text-2xl'>This is our Tech Space.</p>
+            <button className='py-3 px-6 sm:w-[60%] my-4'>Get Started</button>
+
+
+            </div>
+
+            <div>
+                <img className='w-full' src={bgImage} alt="header image" />
+            </div>
+            
+           
+
+
+           
+
+        </div>
+
+    </div>
+  )
+}
+
+export default hero
