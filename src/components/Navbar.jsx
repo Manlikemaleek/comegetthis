cafc7a8a1f2d4f60f1ee5d7091512117114dd471
manlikemaleek <malikagboola3@gmail.com>
2022-04-24 23:40:41 +0100

    finallll

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -19,11 +19,11 @@ const Navbar = () => {
     <div className='flex items-center '>
         <h1 className='text-jxl font-bold mr-4 sm:text-4xl'>CLOUD.</h1>
     <ul className='hidden md:flex'>
-        <li> <Link to="home" smooth={true}  duration={500}>Home</Link> </li> 
-        <li> <Link to="about" smooth={true} offset={-200} duration={500}>About</Link> </li>
-        <li> <Link to="support" smooth={true} offset={-50} duration={500}>Support</Link> </li>
-        <li> <Link to="platforms" smooth={true} offset={-100} duration={500}>Platforms</Link> </li>
-        <li> <Link to="pricing" smooth={true} offset={-50} duration={500}>Pricing</Link> </li>
+        <li className='cursor-pointer'> <Link to="home" smooth={true}  duration={500}>Home</Link> </li> 
+        <li className='cursor-pointer'> <Link to="about" smooth={true} offset={-200} duration={500}>About</Link> </li>
+        <li className='cursor-pointer'> <Link to="support" smooth={true} offset={-50} duration={500}>Support</Link> </li>
+        <li className='cursor-pointer'> <Link to="platforms" smooth={true} offset={-100} duration={500}>Platforms</Link> </li>
+        <li className='cursor-pointer'> <Link to="pricing" smooth={true} offset={-50} duration={500}>Pricing</Link> </li>
     </ul>
     </div>
 
@@ -47,10 +47,10 @@ const Navbar = () => {
 
     <ul className= {!nav ? 'hidden' :'absolute bg-zinc-200 w-full px-8 '}>
     <li> <Link to="home" smooth={true}  duration={500}>Home</Link> </li> 
-        <li  className='border-b-2 border-zinc-300'> <Link onClick={handleClose} to="about" smooth={true} offset={-200} duration={500}>About</Link> </li>
-        <li className='border-b-2 border-zinc-300'> <Link onClick={handleClose} to="support" smooth={true} offset={-50} duration={500}>Support</Link> </li>
-        <li className='border-b-2 border-zinc-300'> <Link onClick={handleClose} to="platforms" smooth={true} offset={-100} duration={500}>Platforms</Link> </li>
-        <li className='border-b-2 border-zinc-300'> <Link onClick={handleClose} to="pricing" smooth={true} offset={-50} duration={500}>Pricing</Link> </li>
+        <li  className='border-b-2 border-zinc-300 cursor-pointer'> <Link onClick={handleClose} to="about" smooth={true} offset={-200} duration={500}>About</Link> </li>
+        <li className='border-b-2 border-zinc-300 cursor-pointer'> <Link onClick={handleClose} to="support" smooth={true} offset={-50} duration={500}>Support</Link> </li>
+        <li className='border-b-2 border-zinc-300 cursor-pointer'> <Link onClick={handleClose} to="platforms" smooth={true} offset={-100} duration={500}>Platforms</Link> </li>
+        <li className='border-b-2 border-zinc-300 cursor-pointer'> <Link onClick={handleClose} to="pricing" smooth={true} offset={-50} duration={500}>Pricing</Link> </li>
         <div className='flex flex-col my-4'>
             <button className='bg-transparent text-indigo-600 px-8 py-3 mb-4 '>Sign In</button>
             <button className='px-8 py-3 '>Sign Up</button>
